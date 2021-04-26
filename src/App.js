a56ed392b6d0222dc6f50934cc718ccac7525df3
Soeren Dolling <soeren.dolling@gmail.com>
2021-04-26 15:03:23 +0200

    Create sign in page basic component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import SignInPage from "./components/SignInPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SignInPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SignInPage.js
@@ -0,0 +1,11 @@
+import "./SignInPage.css";
+
+export default function SignInPage() {
+  return (
+    <div>
+      <h1>name</h1>
+      <input type="text" />
+      <button>Sign in</button>
+    </div>
+  );
+}
