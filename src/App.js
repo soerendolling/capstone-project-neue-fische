13eeeb00ee1a3392216ad817013ba5a0ce0eeb86
Soeren Dolling <soeren.dolling@gmail.com>
2021-04-27 11:50:18 +0200

    Installes React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignInPage from "./components/SignInPage";
+import BookmarkPage from "./components/BookmarkPage";
 
 function App() {
   return (
-    <div className="App">
-      <SignInPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <SignInPage />
+          </Route>
+          <Route exact path="/BookmarkPage">
+            <BookmarkPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/BookmarkPage.js
+++ src/components/BookmarkPage.js
@@ -1,1 +1,11 @@
-import "BookmarkPage.css";
+import "./BookmarkPage.css";
+
+export default function BookmarkPage() {
+  return (
+    <div>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
