d293509bb10db43387086faf44087efcd30cf7b7
Soeren Dolling <soeren.dolling@gmail.com>
2021-04-27 10:46:38 +0200

    Change headline to svg

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -4,20 +4,30 @@ export default function SignInPage() {
   return (
     <div className="app-grid">
       <header className="header-content">
-        <p className="signIn-headline">name</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="signIn-headline__svg"
+        >
+          <g stroke="var(--dark-color)">
+            <text fontFamily="Pegasus" x="140" y="100">
+              name
+            </text>
+          </g>
+        </svg>
+        ;
       </header>
       <main>
         <form className="signIn-image">
           <input
             className="signIn-input"
             type="text"
-            placeholder="Fill in your name"
+            placeholder="fill in your name"
           />
         </form>
       </main>
       <footer>
         <button className="signIn-button" type="submit">
-          Sign in
+          sign in
         </button>
       </footer>
     </div>
