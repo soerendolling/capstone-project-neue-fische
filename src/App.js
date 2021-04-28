63e3ef2445d8ab13b319d995e5c587d264de7e39
Soeren Dolling <soeren.dolling@gmail.com>
2021-04-28 10:38:27 +0200

    Add link to TakeAwayPage

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Route exact path="/bookmarkPage">
             <BookmarkPage />
           </Route>
+          <Route exact path="/takeAwayPage"></Route>
         </Switch>
       </div>
     </Router>
