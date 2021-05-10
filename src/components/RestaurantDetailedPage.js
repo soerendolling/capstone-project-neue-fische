16c7ff3687d0539d93039d9f6fdd66101fe19aa8
Soeren Dolling <soeren.dolling@gmail.com>
2021-05-10 17:26:02 +0200

    Made Restaurant Detail Page Dynamically

--- src/components/RestaurantDetailedPage.js
+++ src/components/RestaurantDetailedPage.js
@@ -10,15 +10,24 @@ import { ReactComponent as Pin } from "../icons/pin.svg";
 import { ReactComponent as Phone } from "../icons/phone.svg";
 import { ReactComponent as Heart } from "../icons/heart-full.svg";
 import { ReactComponent as NoSmoking } from "../icons/no-smoking.svg";
-// import { ReactComponent as WWW } from "../icons/internet.svg";
+import { ReactComponent as WWW } from "../icons/internet.svg";
 import { ReactComponent as LeftArrow } from "../icons/arrow-left-thin.svg";
 import { ReactComponent as Vegetarian } from "../icons/vegetarian.svg";
+import { ReactComponent as Delivery } from "../icons/delivery.svg";
+import { ReactComponent as Lunch } from "../icons/lunch.svg";
+import { ReactComponent as Size } from "../icons/size.svg";
+import { ReactComponent as Euro } from "../icons/euro.svg";
 
 export default function RestaurantDetailedPage({ restaurantData }) {
   let { id } = useParams();
   const singleRestaurant = restaurantData.find(
     (restaurant) => restaurant.id === Number(id)
   );
+
+  let currentDate = new Date();
+  let options = { weekday: "long" };
+  const getDay = new Intl.DateTimeFormat("en-US", options).format(currentDate);
+  const today = getDay.toLowerCase();
 
   let classForMonday;
   let classForTuesday;
@@ -56,6 +65,199 @@ export default function RestaurantDetailedPage({ restaurantData }) {
     ? (classForSunday = " open")
     : (classForSunday = " closed");
 
+  function showMeatlessOptions() {
+    const vegetarian = singleRestaurant.meatless.vegetarianOptions;
+    const vegan = singleRestaurant.meatless.veganOptions;
+    const showMeatlessOptions =
+      vegetarian && vegan
+        ? "vegetarian & vegan"
+        : vegan
+        ? "vegan options"
+        : vegetarian
+        ? "vegetarian options"
+        : "no meatless options";
+
+    return (
+      <span className="info-line">
+        <Vegetarian className="info-svg" />
+        <p className="info-text">{showMeatlessOptions}</p>
+      </span>
+    );
+  }
+
+  function showOpeningTimes() {
+    const openingToday = singleRestaurant.openingTimes[today].general.open;
+    const closingToday = singleRestaurant.openingTimes[today].general.close;
+    const openingTimesInfo =
+      openingToday > 0 ? `${openingToday} - ${closingToday}` : "Closed Today";
+    return (
+      <span className="info-line">
+        <Clock className="info-svg" />
+        <p className="info-text">{openingTimesInfo}</p>
+      </span>
+    );
+  }
+
+  function showLunch() {
+    const lunchTodayOpen = singleRestaurant.openingTimes[today].lunch.open;
+    const lunchTodayClose = singleRestaurant.openingTimes[today].lunch.close;
+    if (lunchTodayOpen > 0) {
+      return (
+        <span className="info-line">
+          <Lunch className="info-svg" />
+          <p className="info-text">{`${lunchTodayOpen} - ${lunchTodayClose}`}</p>
+        </span>
+      );
+    }
+  }
+
+  function showView() {
+    const view = singleRestaurant.location.view;
+    if (view !== "") {
+      return (
+        <span className="info-line">
+          <Binoculars className="info-svg" />
+          <p className="info-text">{view}</p>
+        </span>
+      );
+    }
+  }
+
+  function showTakeAwayOptions() {
+    const takeAwayOptions = singleRestaurant.takeAway;
+    const takeAwayDetails = singleRestaurant.takeAwayDetails;
+    const takeAwayOpen = singleRestaurant.openingTimes[today].takeAway.open;
+    const takeAwayClose = singleRestaurant.openingTimes[today].takeAway.close;
+    const takeAwayInfo = `${takeAwayDetails} ${
+      takeAwayOpen ? takeAwayOpen : ""
+    }  ${takeAwayClose ? takeAwayClose : ""}`;
+    if (takeAwayOptions) {
+      return (
+        <span className="info-line">
+          <TakeAway className="info-svg" />
+          <p className="info-text">{takeAwayInfo}</p>
+        </span>
+      );
+    }
+  }
+
+  function showDelivery() {
+    const delivery = singleRestaurant.delivery;
+    const deliveryDetails = singleRestaurant.deliveryDetails;
+    const deliveryInfo = deliveryDetails;
+    if (delivery) {
+      return (
+        <span className="info-line">
+          <Delivery className="info-svg" />
+          <p className="info-text">{deliveryInfo}</p>
+        </span>
+      );
+    }
+  }
+
+  function showOutdoor() {
+    const outdoorDetail = singleRestaurant.location.restaurantOutdoorDetail;
+    const outdoorSeating = singleRestaurant.location.restaurantOutdoor;
+    const outdoorSeatingBar = singleRestaurant.location.barOutdoor;
+    if (outdoorSeating || outdoorSeatingBar) {
+      const outdoorInfo = !outdoorSeating ? "At the Bar" : outdoorDetail;
+      return (
+        <span className="info-line">
+          <Terrace className="info-svg" />
+          <p className="info-text">{outdoorInfo}</p>
+        </span>
+      );
+    }
+  }
+
+  function showAdress() {
+    const adress = singleRestaurant.contact.adress;
+    const adressLink = `https://www.google.com/maps/search/${singleRestaurant.name}${adress}`;
+    if (adress !== "") {
+      return (
+        <span className="info-line">
+          <Pin className="info-svg" />
+          <a href={adressLink}>
+            <p className="info-text">{adress}</p>
+          </a>
+        </span>
+      );
+    }
+  }
+
+  function showNumber() {
+    const number = singleRestaurant.contact.number;
+    if (number !== "") {
+      return (
+        <span className="info-line">
+          <Phone className="info-svg" />
+          <a href={`tel://${number}`}>
+            <p className="info-text">{singleRestaurant.contact.number}</p>
+          </a>
+        </span>
+      );
+    }
+  }
+
+  function showSmoking() {
+    const smokingInside = singleRestaurant.smoking.inside;
+    const smokingOutside = singleRestaurant.smoking.outside;
+    const smoking = `${
+      smokingInside
+        ? "smoking allowed"
+        : smokingOutside
+        ? "smoking outside allowed"
+        : "none smoking"
+    }`;
+
+    return (
+      <span className="info-line">
+        <NoSmoking className="info-svg" />
+        <p className="info-text">{smoking}</p>
+      </span>
+    );
+  }
+
+  function showSize() {
+    const size = singleRestaurant.size;
+    if (size !== "") {
+      return (
+        <span className="info-line">
+          <Size className="info-svg" />
+          <p className="info-text">{size}</p>
+        </span>
+      );
+    }
+  }
+
+  function showPrice() {
+    const priceTop = singleRestaurant.averagePrice.top;
+    const priceBottom = singleRestaurant.averagePrice.bottom;
+    const priceInfo = `${priceBottom} - ${priceTop}`;
+    if (priceTop > 0) {
+      return (
+        <span className="info-line">
+          <Euro className="info-svg" />
+          <p className="info-text">{priceInfo}</p>
+        </span>
+      );
+    }
+  }
+
+  function showWebsite() {
+    const website = singleRestaurant.contact.website;
+    if (website !== "") {
+      return (
+        <span className="info-line">
+          <WWW className="info-svg" />
+          <a href={website}>
+            <p className="info-text">{website}</p>
+          </a>
+        </span>
+      );
+    }
+  }
+
   return (
     <div className="RestaurantDetailedPage">
       <header className="detailed-header">
@@ -68,9 +270,8 @@ export default function RestaurantDetailedPage({ restaurantData }) {
       <main>
         <h1 className="detailed-heading">{singleRestaurant.name}</h1>
         <div className="detailed-subheading">
-          <h2>{singleRestaurant.cuisine[0]}</h2>
-          <h2>-</h2>
-          <h2>{singleRestaurant.ambience[0]}</h2>
+          <h2>{` ${singleRestaurant.cuisine[0]}  ${singleRestaurant.cuisine[1]} `}</h2>
+          <h2>{` ${singleRestaurant.ambience[0]}  ${singleRestaurant.ambience[1]} `}</h2>
         </div>
         <div className="headline-weekday__layout">
           <div className={`headline-weekday__day${classForMonday}`}>M</div>
@@ -83,43 +284,21 @@ export default function RestaurantDetailedPage({ restaurantData }) {
         </div>
         <div className="detailed-info__layout">
           <div className="detailed-info__text-layout">
-            <span className="info-line">
-              <Clock className="info-svg" />
-              <p className="info-text">open</p>
-            </span>
-
-            <span className="info-line">
-              <Terrace className="info-svg" />
-              <p className="info-text">
-                {singleRestaurant.location.restaurantOutdoorDetailed}
-              </p>
-            </span>
-            <span className="info-line">
-              <Binoculars className="info-svg" />
-              <p className="info-text">{singleRestaurant.location.view}</p>
-            </span>
-            <span className="info-line">
-              <NoSmoking className="info-svg" />
-              <p className="info-text">smoking outside</p>
-            </span>
+            {showLunch()}
+            {showOpeningTimes()}
+            {showOutdoor()}
+            {showView()}
+            {showSmoking()}
+            {showPrice()}
           </div>
           <div className="detailed-info__text-layout">
-            <span className="info-line">
-              <TakeAway className="info-svg" />
-              <p className="info-text">pick-up</p>
-            </span>
-            <span className="info-line">
-              <Vegetarian className="info-svg" />
-              <p className="info-text">veg</p>
-            </span>
-            <span className="info-line">
-              <Pin className="info-svg" />
-              <p className="info-text">{singleRestaurant.contact.adress}</p>
-            </span>
-            <span className="info-line">
-              <Phone className="info-svg" />
-              <p className="info-text">{singleRestaurant.contact.number}</p>
-            </span>
+            {showSize()}
+            {showTakeAwayOptions()}
+            {showDelivery()}
+            {showMeatlessOptions()}
+            {showAdress()}
+            {showNumber()}
+            {showWebsite()}
           </div>
         </div>
       </main>
--- src/models/openingTimesFilter.js
+++ src/models/openingTimesFilter.js
@@ -16,19 +16,26 @@ export function openingTimesFilter(restaurants, filters) {
     }
     if (filters.openingTimes?.includes("today")) {
       return restaurant.openingTimes[today].general.open > 0;
-    } else if (filters.openingTimes?.includes("monday")) {
+    }
+    if (filters.openingTimes?.includes("monday")) {
       return restaurant.openingTimes.monday.general.open > 0;
-    } else if (filters.openingTimes?.includes("tuesday")) {
+    }
+    if (filters.openingTimes?.includes("tuesday")) {
       return restaurant.openingTimes.tuesday.general.open > 0;
-    } else if (filters.openingTimes?.includes("wednesday")) {
+    }
+    if (filters.openingTimes?.includes("wednesday")) {
       return restaurant.openingTimes.wednesday.general.open > 0;
-    } else if (filters.openingTimes?.includes("thursday")) {
+    }
+    if (filters.openingTimes?.includes("thursday")) {
       return restaurant.openingTimes.thursday.general.open > 0;
-    } else if (filters.openingTimes?.includes("friday")) {
+    }
+    if (filters.openingTimes?.includes("friday")) {
       return restaurant.openingTimes.friday.general.open > 0;
-    } else if (filters.openingTimes?.includes("saturday")) {
+    }
+    if (filters.openingTimes?.includes("saturday")) {
       return restaurant.openingTimes.saturday.general.open > 0;
-    } else if (filters.openingTimes?.includes("sunday")) {
+    }
+    if (filters.openingTimes?.includes("sunday")) {
       return restaurant.openingTimes.sunday.general.open > 0;
     }
     return true;
