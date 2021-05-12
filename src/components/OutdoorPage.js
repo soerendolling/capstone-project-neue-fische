5f33d29873b3dfe9a499abd191dc16d0eda6f8b0
Soeren Dolling <soeren.dolling@gmail.com>
2021-05-12 10:28:03 +0200

    Add Weather API to my Outdoor Filter Page

--- src/components/OutdoorPage.js
+++ src/components/OutdoorPage.js
@@ -4,13 +4,57 @@ import { ReactComponent as RightArrow } from "../icons/arrow-right-thin.svg";
 import { ReactComponent as LeftArrow } from "../icons/arrow-left-thin.svg";
 import { ReactComponent as Progress } from "../icons/outdoor-progress.svg";
 import { ReactComponent as Sun } from "../icons/sun.svg";
+import { ReactComponent as Cloudy } from "../icons/sun.svg";
+import { ReactComponent as PartlyCloudy } from "../icons/cloud-sun.svg";
+import { ReactComponent as Rain } from "../icons/cloud-rain.svg";
+import { ReactComponent as Thunder } from "../icons/cloud-lightning.svg";
 import { Link, useHistory, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { stringify, parse } from "../utilities/queryString";
 import { toggleValueInArray } from "../models/toggleValueInArray";
 
 export default function OutdoorPage({ restaurantData }) {
   const location = useLocation();
   const history = useHistory();
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    const url =
+      "http://api.weatherapi.com/v1/current.json?key=487e8af21edb4a54b1f142156211105&q=Hamburg&aqi=no";
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((incomingData) => {
+        const newWeather = incomingData.current;
+        setWeather(newWeather);
+        console.log(incomingData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function showWeatherIcon() {
+    if (weather !== undefined) {
+      if (weather.condition.text.includes("Partly cloudy")) {
+        return <PartlyCloudy className="outdoor-weather__svg" />;
+      } else if (weather.condition.text.includes("sunny")) {
+        return <Sun className="outdoor-weather__svg" />;
+      } else if (weather.condition.text.includes("rain")) {
+        return <Rain className="outdoor-weather__svg" />;
+      } else if (weather.condition.text.includes("Overcast")) {
+        return <Cloudy className="outdoor-weather__svg" />;
+      } else if (weather.condition.text.includes("thunder" || "lightning")) {
+        return <Thunder className="outdoor-weather__svg" />;
+      }
+    }
+  }
+
+  function showTemp() {
+    if (weather !== undefined) {
+      return weather.temp_c;
+    }
+  }
 
   const parsedQueryString = parse(location.search);
   const selectedFilters = parsedQueryString.outdoor || [];
@@ -41,8 +85,8 @@ export default function OutdoorPage({ restaurantData }) {
       </header>
       <main className="outdoor-main">
         <div className="outdoor-weather__layout">
-          <Sun className="outdoor-weather__svg" />
-          <p className="outdoor-wheater__text">26° degrees</p>
+          {showWeatherIcon()}
+          <p className="outdoor-wheater__text">{`${showTemp()}° degrees`}</p>
         </div>
         <div className="outdoor-tag__layout">
           <FilterTag
