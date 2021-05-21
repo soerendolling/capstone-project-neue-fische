fad845cd33be741f89855ada4899a80451b83082
Soeren Dolling <soeren.dolling@gmail.com>
2021-05-21 16:24:56 +0200

    Create WeekdayDisplay Component for Restaurant Detail Page

--- src/pages/RestaurantDetailedPage.js
+++ src/pages/RestaurantDetailedPage.js
@@ -21,6 +21,7 @@ import { ReactComponent as Michelin } from "../icons/michelin.svg";
 import { displayTime } from "../utilities/displayTime";
 import GoBackButton from "../components/GoBackButton";
 import MainButton from "../components/MainButton";
+import WeekdayDisplay from "../components/restaurantDetailed/WeekdayDisplay";
 
 export default function RestaurantDetailedPage({ restaurantData }) {
   let { id } = useParams();
@@ -32,42 +33,6 @@ export default function RestaurantDetailedPage({ restaurantData }) {
   let options = { weekday: "long" };
   const getDay = new Intl.DateTimeFormat("en-US", options).format(currentDate);
   const today = getDay.toLowerCase();
-
-  let classForMonday;
-  let classForTuesday;
-  let classForWednesday;
-  let classForThursday;
-  let classForFriday;
-  let classForSaturday;
-  let classForSunday;
-
-  singleRestaurant.openingTimes.monday.general.open > 0
-    ? (classForMonday = " open")
-    : (classForMonday = " closed");
-
-  singleRestaurant.openingTimes.tuesday.general.open > 0
-    ? (classForTuesday = " open")
-    : (classForTuesday = " closed");
-
-  singleRestaurant.openingTimes.wednesday.general.open > 0
-    ? (classForWednesday = " open")
-    : (classForWednesday = " closed");
-
-  singleRestaurant.openingTimes.thursday.general.open > 0
-    ? (classForThursday = " open")
-    : (classForThursday = " closed");
-
-  singleRestaurant.openingTimes.friday.general.open > 0
-    ? (classForFriday = " open")
-    : (classForFriday = " closed");
-
-  singleRestaurant.openingTimes.saturday.general.open > 0
-    ? (classForSaturday = " open")
-    : (classForSaturday = " closed");
-
-  singleRestaurant.openingTimes.sunday.general.open > 0
-    ? (classForSunday = " open")
-    : (classForSunday = " closed");
 
   function showMeatlessOptions() {
     const vegetarian = singleRestaurant.meatless.vegetarianOptions;
@@ -325,15 +290,7 @@ export default function RestaurantDetailedPage({ restaurantData }) {
           <h2>{showCuisine()}</h2>
           <h2>{` ${singleRestaurant.ambience[0]}  ${singleRestaurant.ambience[1]} `}</h2>
         </div>
-        <div className="headline-weekday__layout">
-          <div className={`headline-weekday__day${classForMonday}`}>M</div>
-          <div className={`headline-weekday__day${classForTuesday}`}>T</div>
-          <div className={`headline-weekday__day${classForWednesday}`}>W</div>
-          <div className={`headline-weekday__day${classForThursday}`}>T</div>
-          <div className={`headline-weekday__day${classForFriday}`}>F</div>
-          <div className={`headline-weekday__day${classForSaturday}`}>S</div>
-          <div className={`headline-weekday__day${classForSunday}`}>S</div>
-        </div>
+        <WeekdayDisplay singleRestaurant={singleRestaurant} />
         <div className="detailed-info__layout">
           <div className="detailed-info__text-top">
             {showLunch()}
