a9d99bf8a7f2174225110f0451141e2498405b1f
Soeren Dolling <soeren.dolling@gmail.com>
2021-05-21 09:17:57 +0200

    Change displayTime to display 12 instead of 0

--- src/utilities/displayTime.js
+++ src/utilities/displayTime.js
@@ -3,9 +3,12 @@ export function displayTime(open, close) {
   const numberLengthClose = close.toString().length > 2 ? 2 : 0;
   const changeFormatOpen = `${(open % 12).toFixed(numberLengthOpen)}`;
   const changeFormatClose = `${(close % 12).toFixed(numberLengthClose)}`;
+  const displayTwelveOpen = changeFormatOpen === "0" ? "12" : changeFormatOpen;
+  const displayTwelveClose =
+    changeFormatClose === "0" ? "12" : changeFormatClose;
   const amPmOpen = open < 12 ? "am" : "pm";
   const amPmClose = close < 12 ? "am" : "pm";
   return open > 0
-    ? `${changeFormatOpen}${amPmOpen} until ${changeFormatClose}${amPmClose}`
+    ? `${displayTwelveOpen}${amPmOpen} until ${displayTwelveClose}${amPmClose}`
     : "Closed Today";
 }
