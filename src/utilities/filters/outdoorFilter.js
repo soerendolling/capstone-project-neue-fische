b4b3ba7cb6f19281c3275302ad5357baf7913157
Soeren Dolling <soeren.dolling@gmail.com>
2021-05-24 17:00:28 +0200

    Add more logic to OutdoorFilter

--- src/utilities/filters/outdoorFilter.js
+++ src/utilities/filters/outdoorFilter.js
@@ -1,25 +1,53 @@
 export function outdoorFilter(restaurants, filters) {
   return restaurants.filter((restaurant) => {
-    if (filters.outdoor?.includes("terrace")) {
-      return (
-        (restaurant.location.restaurantOutdoor === true &&
-          restaurant.location.restaurantOutdoorDetail.includes("terrace")) ||
-        (restaurant.location.barOutdoor === true &&
-          restaurant.location.barOutdoorDetail.includes("terrace"))
-      );
-    } else if (filters.outdoor?.includes("backyard")) {
-      return restaurant.location.restaurantOutdoorDetail.includes("backyard");
-    } else if (filters.outdoor?.includes("rooftop")) {
-      return (
-        restaurant.location.view.includes("rooftop") ||
-        restaurant.location.restaurantOutdoorDetail.includes("rooftop") ||
-        restaurant.location.barOutdoorDetail.includes("rooftop")
-      );
-    } else if (filters.outdoor?.includes("park")) {
-      return (
-        restaurant.location.restaurantOutdoorDetail.includes("park") ||
-        restaurant.location.barOutdoorDetail.includes("park")
-      );
+    const terrace = filters.outdoor?.includes("terrace");
+    const backyard = filters.outdoor?.includes("backyard");
+    const rooftop = filters.outdoor?.includes("rooftop");
+    const park = filters.outdoor?.includes("park");
+
+    const filterTerrace =
+      (restaurant.location?.restaurantOutdoor === true &&
+        restaurant.location?.restaurantOutdoorDetail.includes("terrace")) ||
+      (restaurant.location?.barOutdoor === true &&
+        restaurant.location?.barOutdoorDetail.includes("terrace"));
+    const filterBackyard =
+      restaurant.location?.restaurantOutdoorDetail.includes("backyard");
+    const filterRooftop =
+      restaurant.location?.view.includes("rooftop") ||
+      restaurant.location?.restaurantOutdoorDetail.includes("rooftop") ||
+      restaurant.location?.barOutdoorDetail.includes("rooftop");
+    const filterPark =
+      restaurant.location?.restaurantOutdoorDetail.includes("park") ||
+      restaurant.location?.barOutdoorDetail.includes("park");
+
+    if (terrace && backyard && rooftop && park) {
+      return filterTerrace || filterBackyard || filterRooftop || filterPark;
+    } else if (terrace && backyard && rooftop) {
+      return filterTerrace || filterBackyard || filterRooftop;
+    } else if (terrace && rooftop && park) {
+      return filterTerrace || filterRooftop || filterPark;
+    } else if (backyard && rooftop && park) {
+      return filterBackyard || filterRooftop || filterPark;
+    } else if (terrace && backyard) {
+      return filterTerrace || filterBackyard;
+    } else if (terrace && park) {
+      return filterTerrace || filterPark;
+    } else if (rooftop && park) {
+      return filterRooftop || filterPark;
+    } else if (backyard && rooftop) {
+      return filterBackyard || filterRooftop;
+    } else if (backyard && park) {
+      return filterBackyard || filterPark;
+    } else if (terrace && rooftop) {
+      return filterTerrace || filterRooftop;
+    } else if (terrace) {
+      return filterTerrace;
+    } else if (backyard) {
+      return filterBackyard;
+    } else if (rooftop) {
+      return filterRooftop;
+    } else if (park) {
+      return filterPark;
     }
     return true;
   });
